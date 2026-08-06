be158a4f65eaeefd36d12484c9308d0522ef562e
Dmitry Zakharov <dzakarov@gmail.com>
2026-08-06 16:25:15 +0100

    works
    Signed-off-by: Dmitry Zakharov <dzakarov@gmail.com>

--- wasm-parser/src/lib.rs
+++ wasm-parser/src/lib.rs
@@ -1,8 +1,12 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse_f64_rust(input: &str) -> f64 {
-    input.parse::<f64>().unwrap_or(f64::NAN)
+pub fn parse_f64_rust(input: &str, count: i32) -> f64 {
+    let mut result:f64 = 0.0;
+    for _ in 0..count {
+        result = input.parse::<f64>().unwrap_or(f64::NAN);
+    }
+    result
 }
 
 #[wasm_bindgen]
